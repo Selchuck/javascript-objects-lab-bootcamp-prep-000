9c32306905ffe593823806d612459586aa50ba84
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-24 13:09:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var object = {key: 'value'};
+
+var recipes = Object.assign({},object)
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]:value} );
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+object[key]= value;
+return object;
+
+}
+
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, object, {key});
+}
